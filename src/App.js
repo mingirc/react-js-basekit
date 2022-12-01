273af5f3d65fc3b9ddec481ba76e919ac3dd58ad
Cemal Mingir <mingir.cemal@gmail.com>
2022-12-01 15:52:20 +0000

    "restoring"

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-    const cml = 'kdmadk'
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Edit <code>src/App.js</code> and save to reload. {cml}
+                    Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link"
